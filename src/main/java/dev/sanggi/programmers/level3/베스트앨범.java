1f71884d7b157e3a426b13735ca473ee54945c7e
sgson <ssg3799@gmail.com>
2020-02-22 23:41:21 +0900

    Solve programmers-level3

--- src/main/java/dev/sanggi/programmers/level3/베스트앨범.java
+++ src/main/java/dev/sanggi/programmers/level3/베스트앨범.java
@@ -1,20 +1,20 @@
 package dev.sanggi.programmers.level3;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class 베스트앨범 {
 	public static void main(String[] args) {
-		solution(new String[]{"classic", "pop", "classic", "classic", "pop"}, new int[]{500, 600, 150, 800, 2500});
+		int[] test = solution(new String[]{"classic", "pop", "classic", "classic", "pop"}, new int[]{500, 600, 150, 800, 2500});
+		for (int i : test) {
+			System.out.println(i);
+		}
 	}
 
 	public static int[] solution(String[] genres, int[] plays) {
-		int[] answer = {};
 
 		Map<String, HashMap<Integer, Integer>> chartMap = new HashMap<>();
-
+		// 장르별로 map에 담음
 		for (int i = 0; i < genres.length; i++) {
 			if (chartMap.get(genres[i]) != null)
 				chartMap.get(genres[i]).put(i, plays[i]);
@@ -23,19 +23,28 @@ public class 베스트앨범 {
 				idMap.put(i, plays[i]);
 				chartMap.put(genres[i], idMap);
 			}
-			System.out.println(i + ") " + genres[i] + ": " + plays[i]);
 		}
 
-		chartMap.keySet().forEach(key -> {
-			chartMap.get(key).entrySet().stream()
-					.sorted((o1, o2) -> (o1.getValue() - o2.getValue()) * -1)
-					.mapToInt()
-		});
+		List<Integer> list = new ArrayList();
+		// child map의 value sum 값으로 parent map 정렬 후
+		// child map을 value 높은 순 정렬
+		// value가 같은 경우 id 값으로 정렬
+		// limit2 후 list에 담음
+		chartMap.keySet().stream()
+				.sorted((o1, o2) -> {
+					int current = chartMap.get(o1).keySet().stream().mapToInt(key -> chartMap.get(o1).get(key)).sum();
+					int next = chartMap.get(o2).keySet().stream().mapToInt(key -> chartMap.get(o2).get(key)).sum();
+					return next - current;
+				}).forEach(key ->
+				chartMap.get(key).entrySet().stream()
+						.sorted((o1, o2) -> {
+							int c = (o1.getValue() - o2.getValue()) * -1;
+							return c == 0 ? o1.getKey().compareTo(o2.getKey()) : c;
+						})
+						.mapToInt(o -> o.getKey())
+						.limit(2)
+						.forEach(i -> list.add(i)));
 
-		System.out.println();
-
-		chartMap.keySet().forEach(key -> System.out.println(key + " : " + chartMap.get(key)));
-
-		return answer;
+		return list.stream().mapToInt(i -> i).toArray();
 	}
 }
