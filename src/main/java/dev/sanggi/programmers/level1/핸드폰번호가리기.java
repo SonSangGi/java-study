5d3fc8c96f1bd480a25429a3385d0cf44b6241e4
sgson <ssg3799@gmail.com>
2020-03-01 19:48:25 +0900

    Solve programmers-level1 [핸드폰번호가리기]

--- src/main/java/dev/sanggi/programmers/level1/핸드폰번호가리기.java
+++ src/main/java/dev/sanggi/programmers/level1/핸드폰번호가리기.java
@@ -6,7 +6,17 @@ public class 핸드폰번호가리기 {
 		System.out.println(a);
 	}
 
+	// 정규식으로
 	public static String solution(String phone_number) {
-		return phone_number.replaceAll("(\\d{4})$", "$1*");
+		return phone_number.replaceAll(".(?=.{4})", "*");
+	}
+
+	// 반복문으로
+	public static String solution2(String phone_number) {
+		String answer = "";
+		for (int i = 0; i < phone_number.length() - 4; i++)
+			answer += "*";
+		answer += phone_number.substring(phone_number.length() - 4);
+		return answer;
 	}
 }
